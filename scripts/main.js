92d9f9beaf541820616e1d94af4c8b49a894f9c0
Felix Trepanier <felixtrepanier@gmail.com>
2024-04-06 11:56:42 -0400

    Add welcome page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,43 @@
+export const moduleName = "coriolis-community-atlas";
+export const showedWelcomePageSettingsKey = "showedWelcome";
+
+Hooks.on("init", async () => {
+  console.log("ici");
+  game.settings.register(moduleName, showedWelcomePageSettingsKey, {
+    name: "Showed welcome journal entry.",
+    scope: "world",
+    config: false,
+    type: Boolean,
+    default: false,
+  });
+});
+
+Hooks.on("ready", async () => {
+  const showedWelcome = await game.settings.get(
+    moduleName,
+    showedWelcomePageSettingsKey,
+    false
+  );
+
+  if (!showedWelcome) {
+    const compendium = game.packs.find(
+      (n) => n.metadata.id == moduleName + ".journals"
+    );
+    if (compendium) {
+      const journal = await compendium.getDocument("gB9XzmSrZaxEbgFG");
+      if (journal) {
+        await journal.show(true);
+        await game.settings.set(moduleName, showedWelcomePageSettingsKey, true);
+      }
+    }
+  }
+});
+
 Hooks.on("renderJournalSheet", (app, html, options) => {
-  if (app?.object?.flags?.core?.sourceId.includes("coriolis-community-atlas")) {
+  if (
+    app?.object?.flags?.core?.sourceId?.includes(moduleName) ||
+    app?.object.pack?.includes(moduleName)
+  ) {
     const content = html.find(".journal-entry-content");
 
     if (!content.hasClass("coriolis-core")) {
